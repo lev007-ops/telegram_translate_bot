5f886dfa6f816a1080e384eeef98ea28e43cb460
Lev <lev_bariakh@icloud.com>
2024-10-22 17:32:06 +0500

    add broadcast

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -3,6 +3,11 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from tgbot.models.models import create_user, User
 from tgbot.filters.admin import AdminFilter
+from aiogram.fsm.context import FSMContext
+from tgbot.config import Config
+from aiogram.types import Message, ReplyKeyboardRemove
+from tgbot.misc.states import BroadcastStates
+from tgbot.services.broadcaster import broadcast_telegram_message
 
 start_router = Router()
 
@@ -23,3 +28,25 @@ async def user_start(message: Message):
 async def count_users(message: Message):
     users = len(User.filter())
     await message.answer(f"Всего пользователей: {users}")
+
+
+@start_router.message(Command("broadcast"))
+async def broadcast(message: Message, state: FSMContext, config: Config):
+    if message.from_user.id not in config.tg_bot.admin_ids:
+        return
+    await state.clear()
+    await message.answer("Введите текст рассылки (отмена - /start)",
+                         reply_markup=ReplyKeyboardRemove())
+    await state.set_state(BroadcastStates.input_message)
+
+
+@start_router.message(BroadcastStates.input_message)
+async def broadcast_input_message(message: Message,
+                                  state: FSMContext, config: Config):
+    if message.from_user.id not in config.tg_bot.admin_ids:
+        return
+    users = [int(user.telegram_id) for user in User.select()]
+    await state.clear()
+    await message.answer("Рассылка начата")
+    await broadcast_telegram_message(users, message)
+    await message.answer("Рассылка завершена. /start")
--- tgbot/services/broadcaster.py
+++ tgbot/services/broadcaster.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot
 from aiogram import exceptions
+from aiogram.types import Message
 
 
 async def send_message(bot: Bot, user_id, text: str,
@@ -25,6 +26,25 @@ async def send_message(bot: Bot, user_id, text: str,
     return False
 
 
+async def send_telegram_message(user_id, message: Message,
+                       disable_notification: bool = False) -> bool:
+    try:
+        await message.copy_to(user_id, disable_notification=disable_notification)
+    except exceptions.TelegramForbiddenError:
+        logging.error(f"Target [ID:{user_id}]: got TelegramForbiddenError")
+    except exceptions.TelegramRetryAfter as e:
+        logging.error(f"Target [ID:{user_id}]: Flood limit is exceeded. "
+                      f"Sleep {e.retry_after} seconds.")
+        await asyncio.sleep(e.retry_after)
+        return await send_telegram_message(user_id, message)  # Recursive call
+    except exceptions.TelegramAPIError:
+        logging.exception(f"Target [ID:{user_id}]: failed")
+    else:
+        logging.info(f"Target [ID:{user_id}]: success")
+        return True
+    return False
+
+
 async def broadcast(bot, users, text) -> int:
     """
     Simple broadcaster
@@ -34,6 +54,20 @@ async def broadcast(bot, users, text) -> int:
     try:
         for user_id in users:
             if await send_message(bot, user_id, text):
+                count += 1
+            # 20 messages per second (Limit: 30 messages per second)
+            await asyncio.sleep(0.05)
+    finally:
+        logging.info(f"{count} messages successful sent.")
+
+    return count
+
+
+async def broadcast_telegram_message(users, message: Message):
+    count = 0
+    try:
+        for user_id in users:
+            if await send_telegram_message(user_id, message):
                 count += 1
             # 20 messages per second (Limit: 30 messages per second)
             await asyncio.sleep(0.05)
